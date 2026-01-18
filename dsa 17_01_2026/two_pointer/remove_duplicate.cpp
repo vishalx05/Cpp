ae86db5af6935828f161db7f0b75b36fca684310
vishalx05 <vishalmauryaa1313@gmail.com>
2026-01-18 22:13:58 +0530

    remove_dup

---
+++ dsa 17_01_2026/two_pointer/remove_duplicate.cpp
@@ -0,0 +1,17 @@
+/*
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+*/
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int brute(vector<int>arr){
+    int n=arr.size();
+
+    
+}
+int main(){
+    vector<int>arr;
+
+}
