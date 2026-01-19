acc070150af400b4703667ca3c14c77244285710
vishalx05 <vishalmauryaa1313@gmail.com>
2026-01-19 22:16:26 +0530

    squaring array

--- dsa 17_01_2026/two_pointer/remove_duplicate.cpp
+++ dsa 17_01_2026/two_pointer/remove_duplicate.cpp
@@ -6,12 +6,29 @@ Output: 2, nums = [1,2,_]
 using namespace std;
 #include<vector>
 
-int brute(vector<int>arr){
+int optimal(vector<int>&arr){
     int n=arr.size();
+    int off=0;
+    int cm=1;
+    int no=1;
+    while(cm<n){
+        if(arr[cm]==arr[cm-1]){
+            cm++;
+            continue;
+        }
+        arr[off+1]=arr[cm];
+        off+=1;
+        no++;cm++;
 
-    
+
+    }
+    return no;
+
 }
+
 int main(){
-    vector<int>arr;
+    vector<int>arr={1, 2, 2, 3, 4, 4, 4, 5, 5};
+    cout<<optimal(arr);
+   
 
 }
